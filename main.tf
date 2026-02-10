ca78d9dea31cd56cf156f631a990fb410390599d
ojt-smic-seanalexandermorales <i.sean.morales@sminvestments.com>
2026-02-10 14:25:53 +0800

    Updated main.tf

--- main.tf
+++ main.tf
@@ -18,10 +18,10 @@ resource "aws_security_group" "alb_sg" {
   }
 
   egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    security_groups = ["sg-0093318a732772b84"]
   }
 
   tags = {
