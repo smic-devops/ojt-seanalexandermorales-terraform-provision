ef762fe817ec5f7bfcd554e0faa1137569142ec4
ojt-smic-seanalexandermorales <i.sean.morales@sminvestments.com>
2026-02-10 14:47:45 +0800

    Updated backend.tf

--- backend.tf
+++ backend.tf
@@ -1,7 +1,7 @@
-# terraform {
- #   backend "s3" {
- #       bucket = "itss-devops-ojt-tfstate"
- #       key    = "tfstate-devops-ojt-morales"
- #       region = "ap-southeast-1"
- #   }
-# }
+terraform {
+    backend "s3" {
+        bucket = "itss-devops-ojt-tfstate"
+        key    = "tfstate-devops-ojt-morales"
+        region = "ap-southeast-1"
+    }
+}
